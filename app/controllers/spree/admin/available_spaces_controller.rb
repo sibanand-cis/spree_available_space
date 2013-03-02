4ca5876087951efcd794f0961654a5e0a2578429
cis <cis@CISM257.(none)>
2013-03-02 20:37:21 +0530

    changes on graph

--- app/controllers/spree/admin/available_spaces_controller.rb
+++ app/controllers/spree/admin/available_spaces_controller.rb
@@ -11,19 +11,18 @@ class Spree::Admin::AvailableSpacesController <  Spree::Admin::BaseController
 # end	
 
 # end
-def index
-        @bookings_date = Array.new
-        @booking_rate = Array.new
-        if !params[:search].blank? 
-        if !params[:search]["start_date"].blank? && !params[:search]["end_date"].blank?
-         @bookings =Spree::Booking.where(:pickup_date => (params[:search]["start_date"].to_date)..(params[:search]["end_date"].to_date)).where("rating is not null")
-        else
-         @bookings = []
-        end  
-     end             
-        @bookings.each do |booking|
+  def index
+      @bookings_date = Array.new
+      @booking_rate = Array.new
+      if !params[:search].blank? && !params[:search]["start_date"].blank? && !params[:search]["end_date"].blank?
+       @bookings =Spree::Booking.where(:pickup_date => (params[:search]["start_date"].to_date)..(params[:search]["end_date"].to_date)).where("rating is not null")
+      else
+       @bookings = Spree::Booking.where('pickup_date BETWEEN ? AND ? AND rating IS NOT NULL', DateTime.now.in_time_zone.beginning_of_month, DateTime.now.in_time_zone.end_of_month)
+      end  
+      
+      @bookings.each do |booking|
         @bookings_date << booking.pickup_date
         @booking_rate << booking.rating
-         end unless @bookings.blank?
-      end        
-    end
+      end unless @bookings.blank?
+  end        
+end
