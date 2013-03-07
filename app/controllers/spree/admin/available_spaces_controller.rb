0aaa77ea2908ed4e88c0b98f831ee6c9c01b6611
cis <cis@CISM257.(none)>
2013-03-07 17:07:01 +0530

    chart

--- app/controllers/spree/admin/available_spaces_controller.rb
+++ app/controllers/spree/admin/available_spaces_controller.rb
@@ -1,28 +1,22 @@
 class Spree::Admin::AvailableSpacesController <  Spree::Admin::BaseController
 
-# def index
-# 	@bookings_date = Array.new
-# 	@booking_rate = Array.new
-# 	@bookings =Spree::Booking.where('pickup_date BETWEEN ? AND ? AND rating IS NOT NULL', DateTime.now.in_time_zone.beginning_of_month, DateTime.now.in_time_zone.end_of_month)
-# 	@bookings.each do |booking|
-# 		@bookings_date << booking.pickup_date
-# 		@booking_rate << booking.rating
-# 		end	unless @bookings.blank?
-# end	
 
-# end
   def index
-      @bookings_date = Array.new
-      @booking_rate = Array.new
-      if !params[:search].blank? && !params[:search]["start_date"].blank? && !params[:search]["end_date"].blank?
-       @bookings =Spree::Booking.where(:pickup_date => (params[:search]["start_date"].to_date)..(params[:search]["end_date"].to_date)).where("rating is not null")
+      @bookings_data = {}
+      @booking_rate = []
+
+      start_date = ((params[:search].blank? ? Time.now.beginning_of_month : (params[:search][:start_date].blank? ? Time.now.beginning_of_month : params[:search][:start_date]))).to_date.strftime "%Y-%m-%d"
+
+       
+      end_date = ((params[:search].blank? ? Time.now.end_of_month : (params[:search][:end_date].blank? ? Time.now.end_of_month : params[:search][:end_date])) ).to_date.strftime "%Y-%m-%d"
+
+      if Rails.env == "development"
+       @bookings = Spree::Booking.find_by_sql("SELECT IFNULL(sum( rating ),0) as total_space,filter_date.id as day FROM spree_bookings RIGHT JOIN (select adddate(\"#{start_date}\", numslist.id) as id from (SELECT n1.col_i + n10.col_i*10 + n100.col_i*100 AS id FROM nums n1 cross join nums as n10 cross join nums as n100) as numslist where adddate(\"#{start_date}\", numslist.id) <= \"#{end_date}\" ) filter_date ON (DATE_FORMAT(spree_bookings.pickup_date,'%Y-%m-%d') = filter_date.id) GROUP BY filter_date.id")
       else
-       @bookings = Spree::Booking.where('pickup_date BETWEEN ? AND ? AND rating IS NOT NULL', DateTime.now.in_time_zone.beginning_of_month, DateTime.now.in_time_zone.end_of_month)
-      end  
-      
-      @bookings.each do |booking|
-        @bookings_date << booking.pickup_date
-        @booking_rate << booking.rating
-      end unless @bookings.blank?
+        @bookings = Spree::Booking.find_by_sql("SELECT  filter_date.id as day,coalesce(sum(users.id),0) as total_space FROM  users  RIGHT JOIN ( select \"#{start_date}\"::date + cast(numlist.id::text || ' days' as interval) as  id  from (SELECT n1.i + n10.i*10 + n100.i*100 AS id FROM num n1 cross join num as n10 cross join num as n100) as numlist where (select \"#{start_date}\"::date + cast(numlist.id::text || ' days' as interval)) <= \"#{end_date}\"::date ) filter_date ON ( users.reated_at::date  = filter_date.id) GROUP BY filter_date.id")        
+      end        
+      @bookings.each do |booking|        
+        @bookings_data[booking.day.to_s] = booking.total_space.to_i
+      end unless @bookings.blank?                   
   end        
 end
